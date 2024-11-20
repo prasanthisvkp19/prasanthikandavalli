e830aa0182b16aa9083f682317a9f1efa4945afb
LPT-12 <LPT-12@192.168.1.48>
2024-11-20 01:25:29 -0800

    first commit

---
+++ SVKP2/src/basicsofjava/SimpleIf.java
@@ -0,0 +1,19 @@
+package basicsofjava;
+
+public class SimpleIf {
+
+	public static void main(String[] args) {
+		int age=11;
+		if(age>18)
+		{
+			System.out.println("you are eligible");
+		}
+		else
+		{
+			System.out.println("you are not eligible");
+		}
+	
+
+	}
+
+}
