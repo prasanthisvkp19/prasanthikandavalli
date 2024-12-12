de535211cf30a311827d011b423962431dd159a5
LPT-12 <LPT-12@192.168.1.48>
2024-12-12 02:39:06 -0800

    first commit

---
+++ SVKP2/src/basicsofjava/Return.java
@@ -0,0 +1,13 @@
+package basicsofjava;
+
+public class Return {
+
+	public static void main(String[] args) {
+			System.out.println(sum(5,10));
+			}
+		static int sum(int a,int b)
+		{
+			return a+b;
+		}
+
+	}
