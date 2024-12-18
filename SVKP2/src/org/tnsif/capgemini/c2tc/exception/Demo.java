986b23ec548faa299d5e92f30bac90aaa177823c
LPT-12 <LPT-12@192.168.1.49>
2024-12-18 01:17:38 -0800

    first commit

---
+++ SVKP2/src/org/tnsif/capgemini/c2tc/exception/Demo.java
@@ -0,0 +1,18 @@
+package org.tnsif.capgemini.c2tc.exception;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int sum=add(a,b);
+		
+		System.out.println(sum);
+	}
+	public static int add(int a,int b)
+	{
+		return a-b;
+	}
+
+}
