e361d6d345d9f8dc0d81b85ca81680c4a4da903f
LPT-12 <LPT-12@192.168.1.49>
2024-12-18 01:07:11 -0800

    first commit

---
+++ SVKP2/src/org/tnsif/capgemini/c2tc/constuctor/BeforeConstructor.java
@@ -0,0 +1,16 @@
+package org.tnsif.capgemini.c2tc.constuctor;
+class Myclass
+{
+	String color;
+	int cost;
+	String Brand;
+}
+
+public class BeforeConstructor {
+
+	public static void main(String[] args) {
+		Myclass obj=new Myclass();
+		System.out.println(obj.color + " " +obj.cost + " "+obj.Brand);
+	}
+
+}
