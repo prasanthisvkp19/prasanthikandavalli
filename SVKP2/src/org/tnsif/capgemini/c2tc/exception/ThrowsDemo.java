1a25ab47a1c395bb6feb28a78b5a23251afbbe0c
LPT-12 <LPT-12@192.168.1.49>
2024-12-18 02:14:59 -0800

    first commit

---
+++ SVKP2/src/org/tnsif/capgemini/c2tc/exception/ThrowsDemo.java
@@ -0,0 +1,56 @@
+package org.tnsif.capgemini.c2tc.exception;
+
+import java.io.IOException;
+
+
+public class ThrowsDemo {
+	  //prasanna
+		void m1()
+		{
+			System.out.println("No exception in this method");
+		}
+		//harika
+		void m2() throws IOException
+		{
+			throw new IOException("Device error/ file not found");
+		}
+		
+		//prasanthi
+		void m3() throws ArithmeticException,IOException,ClassNotFoundException
+		{
+			throw new IOException("file not found");
+		}
+		
+
+	public static void main(String[] args) {
+		ThrowsDemo obj=new ThrowsDemo();
+		obj.m1();
+		try
+		{
+			obj.m2();
+	
+		}catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		try
+		{
+			obj.m3();		
+		}catch(ArithmeticException e)
+		{
+			System.out.println("ArithmeticException" +e.getMessage());
+		}
+		catch(IOException e)
+		{
+			System.out.println("IOException " +e.getMessage());
+		}
+		catch(ClassNotFoundException e)
+	  {
+		System.out.println("ClassNotFoundException "+e.getMessage());	
+
+	}
+
+		
+	}
+
+}
