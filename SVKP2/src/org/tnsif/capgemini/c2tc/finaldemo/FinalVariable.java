27df30c725fc781d133803b4d5792422141d4841
LPT-12 <LPT-12@192.168.1.49>
2024-12-20 00:52:10 -0800

    first commit

---
+++ SVKP2/src/org/tnsif/capgemini/c2tc/finaldemo/FinalVariable.java
@@ -0,0 +1,14 @@
+package org.tnsif.capgemini.c2tc.finaldemo;
+
+public class FinalVariable {
+
+	public static void main(String[] args) {
+		
+		 int  mark_scored_honey=50;
+		mark_scored_honey=80; //final variable can't be changed
+		System.out.println(mark_scored_honey);
+
+
+	}
+
+}
