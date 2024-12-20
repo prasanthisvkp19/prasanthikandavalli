1098d1b456ede1de21ba0e3059231764c8edb5bc
LPT-12 <LPT-12@192.168.1.49>
2024-12-20 00:57:29 -0800

    first commit

---
+++ SVKP2/src/org/tnsif/capgemini/c2tc/finaldemo/FinalClass.java
@@ -0,0 +1,27 @@
+package org.tnsif.capgemini.c2tc.finaldemo;
+
+ class calc //honey
+{
+	public void show()
+	{
+		System.out.println("AI integration done by honey");
+	}
+}
+
+class adav extends  calc //yash
+{
+	public void show()
+	{
+		System.out.println("AI integration done by yash");
+	}
+}
+
+
+
+public class FinalClass {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
