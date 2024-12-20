66cd6312223aab832b1c167a6c56d42cbb3d0b67
LPT-12 <LPT-12@192.168.1.49>
2024-12-20 02:53:21 -0800

    first commit

---
+++ SVKP2/src/org/tnsif/capgemini/c2tc/interfacedemo/FunctionalInterface1.java
@@ -0,0 +1,10 @@
+package org.tnsif.capgemini.c2tc.interfacedemo;
+
+import java.lang.FunctionalInterface;
+
+@FunctionalInterface
+
+public interface FunctionalInterface1 {
+
+	 void sayHello(String name);
+}
