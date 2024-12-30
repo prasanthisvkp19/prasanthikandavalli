2e14ddfcfb27489dfdaece3e013420f0772186d6
LPT-12 <LPT-12@192.168.1.49>
2024-12-30 02:12:32 -0800

    first commit

---
+++ SVKP2/src/org/tnsif/capgemini/c2tc/lambdaexpressions/Lambdaexpressionsexample1.java
@@ -0,0 +1,24 @@
+package org.tnsif.capgemini.c2tc.lambdaexpressions;
+
+interface Sayable1{  
+    public String say(String name);  
+}  
+
+public class Lambdaexpressionsexample1 {
+
+	public static void main(String[] args) {
+		
+		 // Lambda expression with single parameter.  
+        Sayable1 s1=(name)->{  
+            return "Hello, "+name;  
+        };  
+        System.out.println(s1.say("Honey"));    
+        // You can omit function parentheses    
+        Sayable1 s2= name ->{  
+            return "Hello, "+name;  
+        };  
+        System.out.println(s2.say("Vicky"));  
+    
+	}
+
+}
